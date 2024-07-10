cd932afff9cd81b0521e96ccd84ccf0e78a45129
mustafid kaisalana <mustafidku26@gmail.com>
2024-07-10 15:42:01 +0700

    paginate

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -9,7 +9,7 @@ class ArtikelController extends Controller
 {
     public function index()
     {
-        $artikel = Artikel::orderBy('created_at', 'desc')->paginate(10);
+        $artikel = Artikel::orderBy('created_at', 'desc')->paginate(5);
         $jumlahartikel = Artikel::count();
         return view('artikel', [
             'artikel' => $artikel,
